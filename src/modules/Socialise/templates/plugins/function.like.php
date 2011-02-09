1c6652852a59dcf70387f4338499ec119333925c
Fabian Wuertz <dev@wuertz.org>
2011-02-09 16:29:55 +0100

    Plugin headers updated

--- src/modules/Socialise/templates/plugins/function.like.php
+++ src/modules/Socialise/templates/plugins/function.like.php
@@ -1,24 +1,20 @@
 <?php
 /**
- * Smarty function to display the sharethis social bookmarking dialog
+ * Smarty function to display a facebook like button
  *
  * Available parameters:
  *
- * id - The unique ID to apply to this instance of the ShareThis Link
- * permalink - The URL of the item to submit to the social bookmarking site(s)
- * title - The title of the item to submit to the social bookmarking site(s)
+ * url - The URL of the item to submit to the social bookmarking site(s)
  *
  * Example (for the News module)
-  * {like $url=permalink=$links.permalink title=$info.title}
+ * {like url=$links.permalink title=$info.title}
  *
- * @author      Fabian Wuertz
- * @since       10/02/2010
- * @link        http://fabian.wuertz.org
+ * @link        http://code.zikula.org/socialise
  * @param       array       $params      All attributes passed to this function from the template
  * @param       object      &$smarty     Reference to the Smarty object
  * @return      string      the YIQ display
  */
 function smarty_function_like($params, &$smarty)
 {
-	return ModUtil::func('socialise', 'user', 'like', $params);
+    return ModUtil::func('socialise', 'user', 'like', $params);
 }
--- src/modules/Socialise/templates/plugins/function.twitter.php
+++ src/modules/Socialise/templates/plugins/function.twitter.php
@@ -1,25 +1,21 @@
 <?php
 /**
- * Smarty function to display the sharethis social bookmarking dialog
+ * Smarty function to display a twitter button
  *
  * Available parameters:
  *
- * id - The unique ID to apply to this instance of the ShareThis Link
- * permalink - The URL of the item to submit to the social bookmarking site(s)
+ * url - The URL of the item to submit to the social bookmarking site(s)
  * title - The title of the item to submit to the social bookmarking site(s)
  *
  * Example (for the News module)
-  * {twitter $url=permalink=$links.permalink title=$info.title}
+ * {twitter url=$links.permalink title=$info.title}
  *
- * @author      Fabian Wuertz
- * @since        08/02/2007
- * @link           http://fabian.wuertz.org
+ * @link        http://code.zikula.org/socialise
  * @param       array       $params      All attributes passed to this function from the template
  * @param       object      &$smarty     Reference to the Smarty object
- * @return       string      the YIQ display
+ * @return      string      the YIQ display
  */
 function smarty_function_twitter($params, &$smarty)
 {
-	return ModUtil::func('socialise', 'user', 'twitter', $params);
+    return ModUtil::func('socialise', 'user', 'twitter', $params);
 }
-
